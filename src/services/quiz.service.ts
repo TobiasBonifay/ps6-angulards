ba8e90c3f883ed8ef1cd7ea52c10543f8a6045ab
TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>
2022-02-01 19:14:49 +0100

    TD1 first part done

--- src/services/quiz.service.ts
+++ src/services/quiz.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 import { Quiz } from '../models/quiz.model';
 import { QUIZ_LIST } from '../mocks/quiz-list.mock';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -18,13 +19,16 @@ export class QuizService {
     */
   private quizzes: Quiz[] = QUIZ_LIST;
 
+  private url = "https://raw.githubusercontent.com/NablaT/starter-quiz-two/master/mock-quiz.json\n";
+
   /**
    * Observable which contains the list of the quiz.
    * Naming convention: Add '$' at the end of the variable name to highlight it as an Observable.
    */
   public quizzes$: BehaviorSubject<Quiz[]> = new BehaviorSubject(QUIZ_LIST);
 
-  constructor() {
+  constructor(private http: HttpClient) {
+    this.getQuizzes();
   }
 
   addQuiz(quiz: Quiz) {
@@ -38,6 +42,14 @@ export class QuizService {
     // delete this.quizzes[quiz.name];
     this.quizzes = this.quizzes.filter(q => q != quiz);
     this.quizzes$.next(this.quizzes);
+  }
+
+  getQuizzes() {
+    this.http.get<Quiz[]>(this.url).subscribe(res => {
+        this.quizzes = res;
+        this.quizzes$.next(res);
+      }
+    )
   }
 
   editQuiz(quiz: Quiz) {
